0f75bd5409f6b9ef82e2f8e704fb20022e155aef
saufiroja <saufiroja08@gmail.com>
2022-04-27 21:33:59 +0800

    📄docs: constanta

---
+++ constant.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName = "Muhammad Saufi"
+	const lastName = "Roja"
+
+	fmt.Println(firstName, lastName)
+
+	// multiple constant
+	const (
+		country = "Indonesia"
+		age = 77
+	)
+	fmt.Println(country)
+	fmt.Println(age)
+}
