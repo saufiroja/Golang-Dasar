4da78e2140c766b1e86143df4f34e34635462aa5
saufiroja <saufiroja08@gmail.com>
2022-04-27 22:08:03 +0800

    📄docs: array

---
+++ array.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name [3]string
+	name[0] = "Muhammad"
+	name[1] = "Saufi"
+	name[2] = "Roja"
+
+	fmt.Println(len(name[0]))
+	fmt.Println(name[1])
+	fmt.Println(name[2])
+
+	var value = [3]int{
+		90,
+		80,
+		70,
+	}
+	value[0] = 100
+	
+	fmt.Println(value)
+	fmt.Println(len(value))
+}
