8e789c0aacf2dd25f67c4a49d4555c7bb75c23ee
saufiroja <saufiroja08@gmail.com>
2022-04-27 21:34:29 +0800

    📄docs: variable

---
+++ variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1
+	var name string
+	name = "Muhammad Saufi Roja"
+	fmt.Println(name)
+
+	// 2
+	var name1 = "Muhammad Saufi Roja"
+	fmt.Println(name1)
+
+	// 3
+	name2 := "Muhammad Saufi Roja"
+	name2 = "Indonesia"
+	fmt.Println(name2)
+
+	// Multiple variable
+	var (
+		firstName = "Muhammad Saufi"
+		lastName = "Roja"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
